908df44a7b184c2b3d221f38b77196e11f9c8f9e
Andrii Shtaher <andriishtaher@gmail.com>
2025-08-08 00:12:14 +0200

    feat(auth): fully implement validate_refresh_token

--- routers/auth.py
+++ routers/auth.py
@@ -2,13 +2,19 @@ from datetime import timedelta, timezone, datetime
 from typing import Annotated
 
 from fastapi import APIRouter, HTTPException, Response
-from fastapi.params import Depends
+from fastapi.params import Depends, Cookie
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt, JWTError
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from starlette import status
 
+import crud
+import models
 import schemas
+import security
 from models import RefreshToken
+from schemas import AccessToken
 from settings import settings
 from crud import get_user_by_email
 from database import get_db
@@ -17,6 +23,7 @@ from security import create_token, verify_password
 router = APIRouter()
 
 DpGetDB = Annotated[AsyncSession, Depends(get_db)]
+
 
 @router.post("login/", response_model=schemas.LoginTokens)
 async def login_endpoint(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: DpGetDB, response: Response):
@@ -51,7 +58,7 @@ async def login_endpoint(form_data: Annotated[OAuth2PasswordRequestForm, Depends
         value=refresh_token,
         httponly=True,
         samesite="lax",
-        secure=True,
+        secure=True,  # HTTPS <------------------------------
         expires=refresh_token_obj.expires_at
     )
 
@@ -59,10 +66,33 @@ async def login_endpoint(form_data: Annotated[OAuth2PasswordRequestForm, Depends
         access_token=access_token,
     )
 
-
 async def validate_refresh_token(refresh_token: Annotated[str | None, Cookie()], db: DpGetDB):
+    invalid_token_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Refresh Token was provided")
+
     if not refresh_token:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Refresh Token")
-    user = security.get_current_user(refresh_token, db)
+        raise invalid_token_exception
+
+    result = await db.execute(select(models.RefreshToken).filter(models.RefreshToken.token == refresh_token))
+    refresh_token_obj = result.scalar_one_or_none()
+    if not refresh_token_obj:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Refresh token wasn't provided")
+    try:
+        payload = jwt.decode(token=refresh_token_obj.token, key=settings.SECRET_KEY, algorithms=settings.ALGORITHM)
+        token_exp = payload.get("exp", "")
+        token_type = payload.get("type", "")
+        token_sub = payload.get("sub", "")
+
+        if not token_exp or not token_type or not token_sub:
+            raise invalid_token_exception
+
+        if token_exp < datetime.now(timezone.utc) or token_type != "refresh":
+            raise invalid_token_exception
+
+    except JWTError:
+        raise invalid_token_exception
+
+    user = await crud.get_user_by_email(token_sub, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User from refresh token not exist")
     return user
 
