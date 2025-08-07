13e86e3e31230cb4c71e1b68975932e50dfa2ede
Andrii Shtaher <andriishtaher@gmail.com>
2025-08-07 18:03:11 +0200

    feat(models): implement "users" model with relationship to user_groups

--- models.py
+++ models.py
@@ -23,3 +23,16 @@ class UserGroup(BaseModel):
 
     users = relationship("User", back_populates="user_group")
 
+
+class User(BaseModel):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(100), unique=True, nullable=False)
+    hashed_password = Column(String(150), nullable=False)
+    is_active = Column(Boolean, default=False, nullable=True)
+    created_at = Column(DateTime, server_default="CURRENT_TIMESTAMP")
+    updated_at = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"), onupdate=func.now())
+    group_id = Column(Integer, ForeignKey("user_groups.id"))
+
+    user_group = relationship("UserGroup", back_populates="users")
