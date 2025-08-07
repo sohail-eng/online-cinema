ea7037f0498e16f611163aeb5ac07abcae1e7bbc
Andrii Shtaher <andriishtaher@gmail.com>
2025-08-07 22:53:35 +0200

    feat(schemas): implement TokenPayload and LoginTokens pydantic schemes

---
+++ schemas.py
@@ -0,0 +1,12 @@
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr
+
+
+class TokenPayload(BaseModel):
+    email: EmailStr
+
+class LoginTokens(BaseModel):
+    access_token: str
+    refresh_token: str
+    header: Optional[str] = "bearer"
