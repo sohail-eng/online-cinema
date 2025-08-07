0f21fe64cdb25875913434c004fe049a6ef63323
Andrii Shtaher <andriishtaher@gmail.com>
2025-08-07 18:19:51 +0200

    feat(models): implement relationships for User (user_profile, activation_token, password_reset_token, refresh_token, user_group). Also implement UserProfile model that has relationship to User

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 from enum import Enum
 
-from pydantic import BaseModel
-from sqlalchemy import Column, Integer, ForeignKey, String, Enum as SqlEnum, Boolean, DateTime, func, text
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, ForeignKey, String, Enum as SqlEnum, Boolean, DateTime, func, text, Date
+from sqlalchemy.orm import relationship, declarative_base
+
+Base = declarative_base()
 
 
 class UserGroupEnum(str, Enum):
@@ -15,7 +16,7 @@ class GenderEnum(str, Enum):
     woman = "WOMAN"
 
 
-class UserGroup(BaseModel):
+class UserGroup(Base):
     __tablename__ = "user_groups"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -24,15 +25,34 @@ class UserGroup(BaseModel):
     users = relationship("User", back_populates="user_group")
 
 
-class User(BaseModel):
+class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(100), unique=True, nullable=False)
     hashed_password = Column(String(150), nullable=False)
     is_active = Column(Boolean, default=False, nullable=True)
-    created_at = Column(DateTime, server_default="CURRENT_TIMESTAMP")
-    updated_at = Column(DateTime, server_default=text("CURRENT_TIMESTAMP"), onupdate=func.now())
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
     group_id = Column(Integer, ForeignKey("user_groups.id"))
 
     user_group = relationship("UserGroup", back_populates="users")
+    user_profile = relationship("UserProfile", back_populates="user", uselist=False)
+    activation_token = relationship("ActivationToken", back_populates="user", uselist=False)
+    password_reset_token = relationship("PasswordResetToken", back_populates="user", uselist=False)
+    refresh_token = relationship("RefreshToken", back_populates="user", uselist=False)
+
+
+class UserProfile(Base):
+    __tablename__ = "user_profiles"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    first_name = Column(String(60), nullable=True)
+    last_name = Column(String(60), nullable=True)
+    avatar = Column(String(300), nullable=True)
+    gender = GenderEnum
+    date_of_birth = Column(Date, nullable=True)
+    info = Column(String(200), nullable=True)
+
+    user = relationship("User", back_populates="user_profile")
