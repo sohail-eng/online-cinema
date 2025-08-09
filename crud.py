5cccdc6f5fae57d6945fbd80151d9fbe5560a70e
Andrii Shtaher <andriishtaher@gmail.com>
2025-08-09 15:48:01 +0200

    feat(crud): implement read_movies (with sorting, pagination, filtering etc.)

--- crud.py
+++ crud.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Any, Coroutine, Sequence
 
 from fastapi import Depends, HTTPException
 from sqlalchemy import Select, select
@@ -7,3 +7,62 @@ from starlette import status
 
 import models
 from database import get_db
+from models import Movie
+
+
+async def read_movies(
+        db: AsyncSession,
+        skip: int = 0,
+        limit: int = 20,
+        filter_imdb: int = None,
+        filter_release_year: int = None,
+        sort_release_year: int = None,
+        sort_price: int = None,
+        sort_popularity: int = None,
+        sort_format: str = "desc",
+        search_name: str = None,
+        search_director: str = None,
+        search_actor: str = None,
+        search_description: str = None
+):
+    query = select(models.Movie)
+
+    if search_name:
+        query = query.filter(models.Movie.name.ilike(f"%{search_name}%"))
+
+    if search_director:
+        query = query.filter(models.Movie.directors.any(models.Director.name.ilike(f"%{search_director}%")))
+
+    if search_actor:
+        query = query.filter(models.Movie.stars.any(models.Star.name.ilike(f"%{search_actor}%")))
+
+    if search_description:
+        query = query.filter(models.Movie.description.ilike(f"%{search_description}%"))
+
+    if filter_imdb:
+        query = query.filter(models.Movie.imdb == filter_imdb)
+
+    if filter_release_year:
+        query = query.filter(models.Movie.year == filter_release_year)
+
+    if sort_release_year:
+        if sort_format == "desc":
+            query = query.order_by(models.Movie.year.desc())
+        elif sort_format == "asc":
+            query = query.order_by(models.Movie.year.asc())
+
+    if sort_price:
+        if sort_format == "desc":
+            query = query.order_by(models.Movie.price.desc())
+        elif sort_format == "asc":
+            query = query.order_by(models.Movie.price.asc())
+
+    if sort_popularity:
+        query = query.order_by(models.Movie.votes.desc())
+
+
+    query = query.offset(skip).limit(limit)
+
+    result = await db.execute(query)
+    return result.scalars().all()
+
