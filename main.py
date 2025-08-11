3b1105f5cbaef4ed205965c00179b3bf47d9b865
Andrii Shtaher <andriishtaher@gmail.com>
2025-08-11 18:33:20 +0200

    feat(main): add exception_handler for UserDontHavePermissionError

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from starlette import status
+from starlette.responses import JSONResponse
+
+from database import engine
+from exceptions import UserDontHavePermissionError, MovieNotFoundError, SomethingWentWrongError, CommentNotFoundError
+from models import Base
+from routers import users, movies, auth
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all)
+    yield
 
 from routers import users, movies
 
-app = FastAPI()
 
+@app.exception_handler(UserDontHavePermissionError)
+async def handler_user_dont_have_permission_error(request, exception):
+    return JSONResponse(
+        content="Users have not permissions to delete movies.",
+        status_code=status.HTTP_403_FORBIDDEN
+    )
 
 app.include_router(users.router)
 app.include_router(movies.router)
