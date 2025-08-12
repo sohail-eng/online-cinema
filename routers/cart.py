682aaa13062fc116d1c5bbab6c1853c491e45a40
Andrii Shtaher <andriishtaher@gmail.com>
2025-08-12 15:48:17 +0200

    feat(routers/cart): implement cart_add_item_endpoint

--- routers/cart.py
+++ routers/cart.py
@@ -12,3 +12,21 @@ from exceptions import SomethingWentWrongError, MovieAlreadyIsPurchasedOrInCartE
 from models import Cart
 
 router = APIRouter()
+
+
+@router.post("/cart/add_item/{movie_id}/")
+async def cart_add_item_endpoint(
+        movie_id: int,
+        db: dependencies.DpGetDB,
+        user: dependencies.GetCurrentUser,
+) -> JSONResponse:
+    try:
+        added_item = await crud.cart_add_item(db=db, user_profile=user.user_profile, movie_id=movie_id)
+    except MovieAlreadyIsPurchasedOrInCartError:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Movie is already purchased or in cart.")
+
+    if not isinstance(added_item, dict):
+        raise SomethingWentWrongError
+
+    return JSONResponse(content=f"{added_item.get('detail')}", status_code=status.HTTP_200_OK)
+
