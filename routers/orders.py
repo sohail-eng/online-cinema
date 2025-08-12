93ae9abf8492b7a8bb60e1de927d3da74ef181a4
Andrii Shtaher <andriishtaher@gmail.com>
2025-08-12 22:42:11 +0200

    feat(routers/orders): implement order_list_endpoint with pagination etc.

--- routers/orders.py
+++ routers/orders.py
@@ -12,3 +12,25 @@ from exceptions import SomethingWentWrongError
 
 router = APIRouter()
 
+
+@router.get("/orders/list/", response_model=schemas.OrdersPaginatedSchema)
+async def order_list_endpoint(
+        db: dependencies.DpGetDB,
+        user: dependencies.GetCurrentUser,
+        offset: int = 0,
+        limit: int = 20
+) -> schemas.OrdersPaginatedSchema:
+
+    order_list = await crud.order_list(db=db, user_profile=user.user_profile)
+    if not order_list:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Orders were not found")
+    if not isinstance(order_list, dict):
+        raise SomethingWentWrongError
+
+    return schemas.OrdersPaginatedSchema(
+        items=order_list.get("all_orders"),
+        offset=offset,
+        limit=limit,
+        total_items=order_list.get("total_items")
+    )
+
