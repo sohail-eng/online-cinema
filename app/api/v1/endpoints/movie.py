653bdaee807da9fc677040876b6b5b89c88ceda6
Andrii Shtaher <andriishtaher@gmail.com>
2025-08-14 19:57:44 +0200

    refactor: move all movie endpoints to app\api\v1\endpoints\movie.py

--- app/api/v1/endpoints/movie.py
+++ app/api/v1/endpoints/movie.py
@@ -1,25 +1,27 @@
 from typing import List
 
-from fastapi import APIRouter
+from fastapi import APIRouter, BackgroundTasks
 from starlette import status
 from starlette.responses import JSONResponse
 
-import crud
-import dependencies
-import models
-import schemas
-from crud import get_movie_by_id
-from exceptions import SomethingWentWrongError
-from models import MovieCommentReply, MovieComment, Movie
-from schemas import MovieReadDetail
+from app.crud.movie import read_movies, get_movie_by_id, create_comment, delete_movie, update_movie, movie_create, \
+    delete_comment, reply_comment, like_comment_or_delete_if_exists, like_comment_reply_or_delete_if_exists, \
+    add_movie_to_favorite_or_delete_if_exists, like_or_dislike_movie_and_delete_if_exists, \
+    rate_movie_from_0_to_10_or_delete_rate_if_exists
+from app.models.movie import MovieComment, MovieCommentReply, Movie
+from app.schemas.movie import MovieRead, MovieReadDetail, CommentCreateSchema, CommentReadAfterCreationSchema, \
+    MovieRatingFromZeroToTen, UserMovieRating, MovieCommentReplyCreate, MovieCommentReplyCreatedRead, MovieCreateSchema, \
+    MovieUpdateScheme
+from app.utils.dependencies import DpGetDB, GetCurrentUser
+from app.utils.exceptions import SomethingWentWrongError
 
 router = APIRouter()
 
 
-@router.get("/movies/", response_model=List[schemas.MovieRead])
+@router.get("/movies/", response_model=List[MovieRead])
 async def movies_list_endpoint(
-        db: dependencies.DpGetDB,
-        user: dependencies.GetCurrentUser,
+        db: DpGetDB,
+        user: GetCurrentUser,
         favorite_list: bool = False,
         skip: int = 0,
         limit: int = 20,
@@ -34,7 +36,7 @@ async def movies_list_endpoint(
         search_actor: str = None,
         search_description: str = None
 ):
-    filtered_movies = await crud.read_movies(
+    filtered_movies = await read_movies(
         db=db,
         user_profile=user.user_profile,
         skip=skip,
@@ -59,15 +61,19 @@ async def movies_list_endpoint(
     return filtered_movies
 
 
-@router.get("/movies/{movie_id}/", response_model=schemas.MovieReadDetail)
+@router.get("/movies/{movie_id}/", response_model=MovieReadDetail)
 async def movie_detail_endpoint(
         movie_id: int,
-        db: dependencies.DpGetDB,
-        user: dependencies.GetCurrentUser
+        db: DpGetDB,
+        user: GetCurrentUser
 ) -> MovieReadDetail:
 
-    movie_dict = await get_movie_by_id(movie_id=movie_id, db=db, user_profile=user.user_profile)
-    movie_detail = schemas.MovieReadDetail.model_validate(movie_dict.get("movie"))
+    movie_dict = await get_movie_by_id(
+        movie_id=movie_id,
+        db=db,
+        user_profile=user.user_profile
+    )
+    movie_detail = MovieReadDetail.model_validate(movie_dict.get("movie"))
     movie_detail = movie_detail.model_copy(update={
         "average_rate_in_stars": movie_dict.get("average_rate_in_stars"),
         "in_favorite_by_current_user": movie_dict.get("in_favorite_by_current_user"),
@@ -82,13 +88,13 @@ async def movie_detail_endpoint(
     return movie_detail
 
 
-@router.post("/movies/{movie_id}/delete/")
+@router.delete("/movies/{movie_id}/delete/")
 async def movie_delete_endpoint(
         movie_id: int,
-        db: dependencies.DpGetDB,
-        user: dependencies.GetCurrentUser
+        db: DpGetDB,
+        user: GetCurrentUser
 ) -> JSONResponse:
-    deleted_movie = await crud.delete_movie(movie_id=movie_id, db=db, user_profile=user.user_profile)
+    deleted_movie = await delete_movie(movie_id=movie_id, db=db, user_profile=user.user_profile)
 
     if not isinstance(deleted_movie, dict):
         raise SomethingWentWrongError
@@ -98,62 +104,62 @@ async def movie_delete_endpoint(
 
     return JSONResponse(content=f"{deleted_movie.get('detail')}", status_code=status.HTTP_403_FORBIDDEN)
 
-@router.post("/movies/{movie_id}/update/", response_model=schemas.MovieRead)
+@router.patch("/movies/{movie_id}/update/", response_model=MovieRead)
 async def movie_update_endpoint(
         movie_id: int,
-        db: dependencies.DpGetDB,
-        user: dependencies.GetCurrentUser,
-        data: schemas.MovieUpdateScheme
-) -> models.Movie:
-    updated_movie = await crud.update_movie(movie_id=movie_id, db=db, user_profile=user.user_profile, data=data)
+        db: DpGetDB,
+        user: GetCurrentUser,
+        data: MovieUpdateScheme
+) ->Movie:
+    updated_movie = await update_movie(movie_id=movie_id, db=db, user_profile=user.user_profile, data=data)
 
-    if not isinstance(updated_movie, models.Movie):
+    if not isinstance(updated_movie, Movie):
         raise SomethingWentWrongError
 
     return updated_movie
 
 
-@router.post("/movies/create/", response_model=schemas.MovieReadDetail)
+@router.post("/movies/create/", response_model=MovieReadDetail)
 async def movie_create_endpoint(
-        db: dependencies.DpGetDB,
-        user: dependencies.GetCurrentUser,
-        data: schemas.MovieCreateSchema
+        db: DpGetDB,
+        user: GetCurrentUser,
+        data: MovieCreateSchema
 ) -> Movie:
-    created_movie = await crud.movie_create(db=db, user_profile=user.user_profile, data=data)
+    created_movie = await movie_create(db=db, user_profile=user.user_profile, data=data)
 
-    if not isinstance(created_movie, models.Movie):
+    if not isinstance(created_movie, Movie):
         raise SomethingWentWrongError
 
     return created_movie
 
 
-@router.post("/movies/{movie_id}/create_comment/", response_model=schemas.CommentReadAfterCreationSchema)
+@router.post("/movies/{movie_id}/create_comment/", response_model=CommentReadAfterCreationSchema)
 async def movie_create_comment_endpoint(
         movie_id: int,
-        db: dependencies.DpGetDB,
-        user: dependencies.GetCurrentUser,
-        data: schemas.CommentCreateSchema
+        db: DpGetDB,
+        user: GetCurrentUser,
+        data: CommentCreateSchema
 ) -> MovieComment:
-    created_comment = await crud.create_comment(
+    created_comment = await create_comment(
         movie_id=movie_id,
         db=db,
         user_profile=user.user_profile,
         data=data
     )
 
-    if not isinstance(created_comment, models.MovieComment):
+    if not isinstance(created_comment, MovieComment):
         raise SomethingWentWrongError
 
     return created_comment
 
 
-@router.post("/movies/comments/{comment_id}/delete_comment/")
+@router.delete("/movies/comments/{comment_id}/delete_comment/")
 async def movie_delete_comment_endpoint(
         comment_id: int,
-        db: dependencies.DpGetDB,
-        user: dependencies.GetCurrentUser
+        db: DpGetDB,
+        user: GetCurrentUser
 ) -> JSONResponse:
-    deleted_comment = await crud.delete_comment(comment_id=comment_id, db=db, user_profile=user.user_profile)
+    deleted_comment = await delete_comment(comment_id=comment_id, db=db, user_profile=user.user_profile)
 
     if not isinstance(deleted_comment, dict):
         raise SomethingWentWrongError
@@ -161,20 +167,22 @@ async def movie_delete_comment_endpoint(
     return JSONResponse(content=f"{deleted_comment.get('detail')}", status_code=status.HTTP_200_OK)
 
 
-@router.post("/movies/comments/{comment_id}/comment_reply_create/", response_model=schemas.MovieCommentReplyCreatedRead)
+@router.post("/movies/comments/{comment_id}/comment_reply_create/", response_model=MovieCommentReplyCreatedRead)
 async def movie_comment_reply_endpoint(
         comment_id: int,
-        db: dependencies.DpGetDB,
-        user: dependencies.GetCurrentUser,
-        data: schemas.MovieCommentReplyCreate
+        db: DpGetDB,
+        user: GetCurrentUser,
+        data: MovieCommentReplyCreate,
+        background_task: BackgroundTasks
 ) -> MovieCommentReply:
-    created_reply = await crud.reply_comment(
+    created_reply = await reply_comment(
         comment_id=comment_id,
         db=db,
         user_profile=user.user_profile,
-        data=data
+        data=data,
+        background_task=background_task
     )
-    if not isinstance(created_reply, models.MovieCommentReply):
+    if not isinstance(created_reply, MovieCommentReply):
         raise SomethingWentWrongError
 
     return created_reply
@@ -183,10 +191,10 @@ async def movie_comment_reply_endpoint(
 @router.post("/movies/comments/{comment_id}/like_or_delete_like/")
 async def movie_comment_like_or_delete_like_if_exists_endpoint(
         comment_id: int,
-        db: dependencies.DpGetDB,
-        user: dependencies.GetCurrentUser
+        db: DpGetDB,
+        user: GetCurrentUser
 ) -> JSONResponse:
-    like_or_delete_like = await crud.like_comment_or_delete_if_exists(
+    like_or_delete_like = await like_comment_or_delete_if_exists(
         comment_id=comment_id,
         db=db,
         user_profile=user.user_profile
@@ -201,10 +209,10 @@ async def movie_comment_like_or_delete_like_if_exists_endpoint(
 @router.post("/movies/comments/comment_replies/{reply_id}/like_or_delete_like/")
 async def movie_comment_reply_like_or_delete_like_if_exists_endpoint(
         reply_id: int,
-        db: dependencies.DpGetDB,
-        user: dependencies.GetCurrentUser
+        db: DpGetDB,
+        user: GetCurrentUser
 ) -> JSONResponse:
-    comment_like_or_unlike = await crud.like_comment_reply_or_delete_if_exists(
+    comment_like_or_unlike = await like_comment_reply_or_delete_if_exists(
         comment_reply_id=reply_id,
         db=db,
         user_profile=user.user_profile
@@ -219,10 +227,10 @@ async def movie_comment_reply_like_or_delete_like_if_exists_endpoint(
 @router.post("/movies/{movie_id}/add_to_favorite_or_delete/")
 async def movie_add_to_favorite_or_delete_if_exists_endpoint(
         movie_id: int,
-        user: dependencies.GetCurrentUser,
-        db: dependencies.DpGetDB
+        user: GetCurrentUser,
+        db: DpGetDB
 ) -> JSONResponse:
-    add_to_favorite_or_delete = await crud.add_movie_to_favorite_or_delete_if_exists(
+    add_to_favorite_or_delete = await add_movie_to_favorite_or_delete_if_exists(
         movie_id=movie_id,
         user_profile=user.user_profile,
         db=db
@@ -237,11 +245,11 @@ async def movie_add_to_favorite_or_delete_if_exists_endpoint(
 @router.post("/movies/{movie_id}/like_or_dislike_or_delete/")
 async def movie_like_or_dislike_or_delete_like_or_dislike_endpoint(
         movie_id: int,
-        db: dependencies.DpGetDB,
-        user: dependencies.GetCurrentUser,
-        data: schemas.UserMovieRating
+        db: DpGetDB,
+        user: GetCurrentUser,
+        data: UserMovieRating
 ) -> JSONResponse:
-    like_or_dislike_or_delete = await crud.like_or_dislike_movie_and_delete_if_exists(
+    like_or_dislike_or_delete = await like_or_dislike_movie_and_delete_if_exists(
         movie_id=movie_id,
         user_profile=user.user_profile,
         db=db,
@@ -257,11 +265,11 @@ async def movie_like_or_dislike_or_delete_like_or_dislike_endpoint(
 @router.post("/movies/{movie_id}/rate_from_zero_to_ten/")
 async def movie_rate_from_sero_to_ten_endpoint(
         movie_id: int,
-        db: dependencies.DpGetDB,
-        user: dependencies.GetCurrentUser,
-        data: schemas.MovieRatingFromZeroToTen
+        db: DpGetDB,
+        user: GetCurrentUser,
+        data: MovieRatingFromZeroToTen
 ) -> JSONResponse:
-    rate_movie = await crud.rate_movie_from_0_to_10_or_delete_rate_if_exists(
+    rate_movie = await rate_movie_from_0_to_10_or_delete_rate_if_exists(
         db=db,
         movie_id=movie_id,
         data=data,
